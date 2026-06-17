979397547a5ed30f38c37966dc475823ba19cad8
Sasha Davila <sasha@example.com>
2026-06-17 20:51:04 -0300

    logo

--- nakama-landing/src/components/Navbar.tsx
+++ nakama-landing/src/components/Navbar.tsx
@@ -1,20 +1,57 @@
+import logo from "../assets/images/nakama-logo.svg";
+
+
 function Navbar() {
+
     return (
+
         <header className="navbar">
 
-            <div className="logo">
-                nakama
-            </div>
+
+            <a 
+                href="/" 
+                className="logo"
+            >
+
+                <img
+                    src={logo}
+                    alt="Nakama"
+                />
+
+            </a>
+
+
 
             <nav>
-                <a href="#proyectos">PROYECTOS</a>
-                <a href="#nosotros">NOSOTROS</a>
-                <a href="#trabajamos">CÓMO TRABAJAMOS</a>
-                <a href="#contacto">CONTACTO</a>
+
+                <a href="#proyectos">
+                    COLECCIÓN
+                </a>
+
+
+                <a href="#nosotros">
+                    FILOSOFÍA
+                </a>
+
+
+                <a href="#trabajamos">
+                    CÓMO TRABAJAMOS
+                </a>
+
+
+                <a href="#contacto">
+                    CONTACTO
+                </a>
+
+
             </nav>
 
+
         </header>
+
     );
+
 }
+
 
 export default Navbar;
